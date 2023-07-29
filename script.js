ed873a767fb585e1f8fd3ab4318cd68f1d2eca9d
Vedang <76455633+vedangBh@users.noreply.github.com>
2023-07-28 17:27:13 -0700

    GDPs

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ d3.csv("data/gdp_c.csv").then(function (data) {
       }
     });
   
-    const years = Array.from(Array(61), (_, i) => 1960 + i); 
+    const years = Array.from(Array(61), (_, i) => 1960 + i);
     const xScale = d3.scaleLinear().domain([0, years.length - 1]).range([0, width]);
   
     const slider = d3
@@ -51,25 +51,27 @@ d3.csv("data/gdp_c.csv").then(function (data) {
         .append("circle")
         .attr("cx", (d, i) => i * (width / selectedYearData.length))
         .attr("cy", height / 2)
-        .attr("r", 0) 
+        .attr("r", 0)
         .attr("fill", "steelblue")
         .on("mouseover", function (event, d) {
-          tooltip
-            .select("text")
-            .text(`Country: ${d.CountryCode}\nGDP: ${d.GDP.toFixed(2)} billion USD`);
-          tooltip.style("display", "block");
-        })
+            const mousePos = d3.pointer(event, this); // Get the mouse position relative to the hovered element
+            tooltip
+              .select("text")
+              .style("font-size", "8px")
+              .text(`Code: ${d.CountryCode}\nGDP: ${(d.GDP / 1e12).toFixed(4)} trillion USD`);
+            tooltip.style("display", "block").attr("transform", `translate(${mousePos[0]}, ${mousePos[1] - 100})`);
+          })
         .on("mouseout", function () {
           tooltip.style("display", "none");
         })
-        .merge(circles) 
+        .merge(circles)
         .transition()
         .duration(500)
         .attr("r", (d) => circleScale(d.GDP));
   
       const labels = svg.selectAll(".country-label").data(selectedYearData, (d) => d.CountryCode);
       labels.exit().remove();
-
+  
       labels
         .enter()
         .append("text")
@@ -77,15 +79,73 @@ d3.csv("data/gdp_c.csv").then(function (data) {
         .attr("x", (d, i) => i * (width / selectedYearData.length))
         .attr("y", height / 2 + 40)
         .style("text-anchor", "middle")
-        .style("font-size", "8px") 
+        .style("font-size", "8px")
         .text((d) => d.CountryCode)
-        .merge(labels); 
+        .merge(labels);
   
+      // Calculate the GDP changes for each country
+      const gdpChanges = selectedYearData.map((d) => ({
+        CountryCode: d.CountryCode,
+        GDPChange: d.GDP - d[1960], // Change from the initial year (1960)
+      }));
+  
+      // Find the country with the biggest and smallest GDP changes
+      const maxGDPChange = d3.max(gdpChanges, (d) => d.GDPChange);
+      const minGDPChange = d3.min(gdpChanges, (d) => d.GDPChange);
+  
+      // Filter the countries with the biggest and smallest GDP changes
+      const biggestChangeCountry = gdpChanges.find((d) => d.GDPChange === maxGDPChange);
+      const smallestChangeCountry = gdpChanges.find((d) => d.GDPChange === minGDPChange);
+  
+      // Add annotations for the countries with the biggest and smallest GDP changes
+      const annotations = [
+        {
+          note: {
+            label: `Biggest Change: ${biggestChangeCountry.CountryName}\nGDP Change: ${maxGDPChange.toFixed(
+              2
+            )} trillion USD`,
+            title: "Biggest Change",
+          },
+          x: width / 4, // Position the annotation under the title
+          y: -30,
+          dx: 0,
+          dy: 0,
+          color: "red",
+          type: d3.annotationCallout,
+          subject: {
+            radius: 10,
+          },
+        },
+        {
+          note: {
+            label: `Smallest Change: ${smallestChangeCountry.CountryName}\nGDP Change: ${minGDPChange.toFixed(
+              2
+            )} trillion USD`,
+            title: "Smallest Change",
+          },
+          x: (width / 4) * 3, // Position the annotation under the title
+          y: -30,
+          dx: 0,
+          dy: 0,
+          color: "green",
+          type: d3.annotationCallout,
+          subject: {
+            radius: 10,
+          },
+        },
+      ];
+  
+      // Create a new annotation layer
+      const makeAnnotations = d3.annotation().annotations(annotations);
+  
+      // Remove the existing annotation layer if present
+      svg.selectAll(".annotation-group").remove();
+  
+      // Append the new annotation layer to the SVG
+      svg.append("g").attr("class", "annotation-group").call(makeAnnotations);
     }
-      const tooltip = svg
-      .append("g")
-      .attr("class", "tooltip")
-      .style("display", "none");
+  
+    const tooltip = svg.append("g").attr("class", "tooltip").style("display", "none");
   
     tooltip
       .append("rect")
